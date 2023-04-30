4ac596c23ee9aec7dfd45bf612251406919e194b
Kiryl Shreyter <kirylshreyter@gmail.com>
2023-04-30 17:58:22 +0200

    draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    console.log('constructor');
+    super();
+    this.state = {
+      users: [],
+      search: ""
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then(users => this.setState(() => {
+          return {users: users};
+        },
+        () => {
+          console.log(this.state)
+        }));
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <div className="App">
+        <input type='search' onChange={(event) => {
+          this.setState({search: event.target.value})
+        }}/>
+        {
+          this.state.users.filter((user) => user.name.match(new RegExp(`${this.state.search}`, "i"))).map((user) => this.getDiv(user))
+        }
+        {console.log(this.state)}
+      </div>
+    );
+  }
+
+  getDiv(user) {
+    return <div key={user.id}>
+      <h1>{user.name}</h1>
+    </div>;
+  }
 }
 
 export default App;
